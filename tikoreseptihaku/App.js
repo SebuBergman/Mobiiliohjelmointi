979f808461119163717aed0ac4702e8e427ade45
SebuBergman <basti.bergman@gmail.com>
2022-09-20 16:51:24 +0300

    Reseptihaku valmiina

--- tikoreseptihaku/App.js
+++ tikoreseptihaku/App.js
@@ -1,11 +1,48 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, FlatList, Image, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [keyword, setKeyword] = useState('');
+  const [allRecipes, setAllRecipes] = useState('');
+
+  const getRecipes = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${keyword}`)
+    .then(res => res.json())
+    .then(data => {
+      setAllRecipes(data.meals);
+    })
+    .catch(err => console.error(err));
+    }
+
+  useEffect(() => {
+    getRecipes();
+    console.log(allRecipes);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <View style={styles.innerContainer}>
+          <Text style={styles.heading}>Recipes:</Text>
+          <FlatList
+              keyExtractor={(item, index) => index.toString()}
+              renderItem={({ item }) =>
+                <View>
+                  <Text style={styles.title}>{item.strMeal}</Text>
+                  <Image style={styles.tinyImage} source={{uri: item.strMealThumb,}} />
+                </View>
+              }
+              data={allRecipes} />
+          <StatusBar style="auto" />
+        </View>
+      <View style={styles.searchbar}>
+      <TextInput
+        style={{fontSize: 18, width: 200, borderBottomWidth: 1.0, marginBottom: 5}}
+        placeholder='keyword'
+        onChangeText={text => setKeyword(text) } />
+      <Button title="Find" onPress= {getRecipes} />
+      </View>
     </View>
   );
 }
@@ -13,7 +50,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  innerContainer: {
+    flex: 5,
+    marginTop: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  title: {
+    fontSize: 15,
+  },
+  tinyImage: {
+    width: 120,
+    height: 120,
+    marginBottom: 15,
+  },
+  searchbar: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
